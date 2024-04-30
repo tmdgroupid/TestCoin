8c7b6e6767e3b5c43ddbf8c0a66f2e458e277a7c
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-04-30 19:41:00 +0700

    Fixing bugs function IsConnected

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from solcx import compile_source
 app = Flask(__name__)
 
 # Connect to the local Ethereum network
-webthree = Web3(Web3.HTTPProvider("http://localhost:8545"))
+web3 = Web3(Web3.HTTPProvider("http://localhost:8545"))
 
-# Check if the connection Ethreum is successful
-if webthree.isConnected():
+# Check if the connection to Ethereum is successful
+if web3.eth.defaultAccount:
     print("Connected to Ethereum network!")
 else:
     print("Failed to connect to Ethereum network.")
@@ -22,13 +22,13 @@ bytecode = contract_interface["bin"]
 
 # Deploy the contract
 total_supply = 1000
-contract = webthree.eth.contract(abi=abi, bytecode=bytecode)
+contract = web3.eth.contract(abi=abi, bytecode=bytecode)
 tx_hash = contract.constructor(total_supply).transact()
-tx_receipt = webthree.eth.wait_for_transaction_receipt(tx_hash)
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 contract_address = tx_receipt["contractAddress"]
 
 # Create a new contract instance
-contract = webthree.eth.contract(address=contract_address, abi=abi)
+contract = web3.eth.contract(address=contract_address, abi=abi)
 
 # Define a route to get the total supply
 @app.route("/total_supply")
@@ -45,13 +45,13 @@ def balance(address):
 # Define a route to send TestCoins to an address
 @app.route("/send", methods=["POST"])
 def send():
-    from_address = webthree.eth.defaultAccount
+    from_address = web3.eth.defaultAccount
     to_address = request.form["to"]
     value = int(request.form["value"])
 
     # Send the transaction
     tx_hash = contract.functions.transfer(to_address, value).transact({"from": from_address})
-    tx_receipt = webthree.eth.wait_for_transaction_receipt(tx_hash)
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
     # Return the transaction hash
     return jsonify({"tx_hash": tx_hash.hex()})

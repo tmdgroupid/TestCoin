b391f428c6665a89f83f03086647ae01537a2f65
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-04-30 19:34:28 +0700

    Update add validation connected network Ethreum

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify, request
-from web3 import Web3, HTTPProvider
+from webthree import webthree, HTTPProvider
 from solcx import compile_source
 
 app = Flask(__name__)
 
 # Connect to the local Ethereum network
-web3 = Web3(HTTPProvider("http://localhost:8545"))
+webthree = webthree(HTTPProvider("http://localhost:8545"))
+
+# Check if the connection Ethreum is successful
+if webthree.isConnected():
+    print("Connected to Ethereum network!")
+else:
+    print("Failed to connect to Ethereum network.")
 
 # Compile the Solidity contract
 contract_source_code = open("TestCoin.sol", "r").read()
@@ -16,13 +22,13 @@ bytecode = contract_interface["bin"]
 
 # Deploy the contract
 total_supply = 1000
-contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+contract = webthree.eth.contract(abi=abi, bytecode=bytecode)
 tx_hash = contract.constructor(total_supply).transact()
-tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+tx_receipt = webthree.eth.wait_for_transaction_receipt(tx_hash)
 contract_address = tx_receipt["contractAddress"]
 
 # Create a new contract instance
-contract = web3.eth.contract(address=contract_address, abi=abi)
+contract = webthree.eth.contract(address=contract_address, abi=abi)
 
 # Define a route to get the total supply
 @app.route("/total_supply")
@@ -39,13 +45,13 @@ def balance(address):
 # Define a route to send TestCoins to an address
 @app.route("/send", methods=["POST"])
 def send():
-    from_address = web3.eth.defaultAccount
+    from_address = webthree.eth.defaultAccount
     to_address = request.form["to"]
     value = int(request.form["value"])
 
     # Send the transaction
     tx_hash = contract.functions.transfer(to_address, value).transact({"from": from_address})
-    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    tx_receipt = webthree.eth.wait_for_transaction_receipt(tx_hash)
 
     # Return the transaction hash
     return jsonify({"tx_hash": tx_hash.hex()})

0f3858782fb2ab5e49905631076b21280a034b9c
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-05-01 15:07:46 +0700

    Update functionally install_solc

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from web3 import Web3
-from solcx import compile_source
+from solcx import compile_source, install_solc
 import json
 
 app = Flask(__name__)
 
-# Install Solidity Compiler (solc) if not installed
-solcx.install_solc('0.8.7')
-print("Solidity compiler (solc) installed successfully.")
+# Install Solidity compiler
+install_solc("0.8.0")
+print("Install Solidity Compiler Successfull")
 
 # Connect to the Ethereum mainnet using Infura
 infura_url = "https://mainnet.infura.io/v3/204b2e25317d4e3c8d59bf61d1830702"  # Replace with your Infura API key
@@ -16,11 +16,11 @@ infura_url = "https://mainnet.infura.io/v3/204b2e25317d4e3c8d59bf61d1830702"  # 
 private_key = "f10d9f7ff5a88325c8a8b8793cdd2523f90002f9d5a1f5cfd38f0268f66efb70"
 
 # Compile Solidity source code
-with open("./TestCoin.sol", "r") as file:
+with open("./RetailCoin.sol", "r") as file:
     source_code = file.read()
 
 compiled_code = compile_source(source_code)
-contract_interface = compiled_code["<stdin>:TestCoin"]
+contract_interface = compiled_code["<stdin>:RetailCoin"]
 
 # Connect to the Ethereum network
 w3 = Web3(Web3.HTTPProvider(infura_url))

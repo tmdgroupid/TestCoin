6432e8404347b7bb6b851c0439af3c6c124646f7
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-05-01 14:48:54 +0700

    Update script become simple script

--- TestCoin.sol
+++ TestCoin.sol
@@ -1,20 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// Import OpenZeppelin Contracts using the correct path
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title TestCoin
- * @dev Simple ERC20 Token example, where all tokens are pre-assigned to the creator.
- */
-contract TestCoin is ERC20, Ownable {
-
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor () ERC20("TestCoin", "TSC") {
-        _mint(msg.sender, 1000 * 10 ** uint256(decimals()));
+contract RetailCoin is ERC20 {
+    constructor() ERC20("RetailCoin", "RTC") {
+        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
     }
 }

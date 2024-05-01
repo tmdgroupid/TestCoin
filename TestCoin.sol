f4bae50ea2fa28d6e9e8e157fe2ceba316b8cc36
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-05-01 14:51:37 +0700

    Update improve code

--- TestCoin.sol
+++ TestCoin.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract RetailCoin is ERC20 {
-    constructor() ERC20("RetailCoin", "RTC") {
+contract TestCoin is ERC20 {
+    constructor() ERC20("TestCoin", "TSC") {
         _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
     }
 }

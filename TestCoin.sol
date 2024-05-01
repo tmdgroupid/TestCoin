2e5f2f8c85e67644103dc4a175f1f128622bd54c
PT Media Pengembangan Teknologi Indonesia Jaya(Tech Media Development)(TMD Group ID) <156943299+tmdgroupid@users.noreply.github.com>
2024-05-01 14:28:50 +0700

    Changed version solidity

--- TestCoin.sol
+++ TestCoin.sol
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
-pragma solidity ^0.8.4;
-
+// Import OpenZeppelin Contracts using the correct path
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title TestCoin
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` functions.
+ * @dev Simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  */
 contract TestCoin is ERC20, Ownable {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20("TestCoin", "TSC") {
-        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
+    constructor () ERC20("TestCoin", "TSC") {
+        _mint(msg.sender, 1000 * 10 ** uint256(decimals()));
     }
 }
